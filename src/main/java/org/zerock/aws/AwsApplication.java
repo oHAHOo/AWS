edc5a6f4c9b43ca486ae5fe4fda8522586f33bfa
hjh <hjh690@naver.com>
2026-05-14 11:45:13 +0900

    LV 1 - 네트워크 구축 및 핵심 기능 배포
    - 애플리케이션 개발 (API & Actuator)
    - **운영 설정**
        - 로그 전략

--- src/main/java/org/zerock/aws/AwsApplication.java
+++ src/main/java/org/zerock/aws/AwsApplication.java
@@ -1,11 +1,8 @@
 package org.zerock.aws;
 
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.*;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
