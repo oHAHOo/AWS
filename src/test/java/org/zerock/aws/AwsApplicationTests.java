c1e7893485a8bbcee6197513c1ec96f645083f5f
hjh <hjh690@naver.com>
2026-05-14 09:22:21 +0900

    Initial commit

---
+++ src/test/java/org/zerock/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
